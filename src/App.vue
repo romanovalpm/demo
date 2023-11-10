fd05f793c4ba89c39a9e29428298eee6b7ac4722
nadezhda <romanovalpm@gmail.com>
2023-11-10 03:33:36 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {RouterLink, RouterView} from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+nav {
+    min-height: 10vh;
+    display: flex;
+    gap: 20px;
+    align-items: center;
+    padding: 20px;
+    box-shadow: 0 2px 3px 0 rgba(0, 0, 0, .06);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    Hello homepage!
+  </main>
+</template>
+
+<style scoped>
+main {
+    padding: 20px;
+}
+</style>
